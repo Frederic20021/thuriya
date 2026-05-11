82d6be26d594cea13f7f9810faca56350c4789e9
Frederic20021 <frederic123.bf@gmail.com>
2026-05-11 18:20:17 +0900

    "added background"

--- app/components/Consultant.tsx
+++ app/components/Consultant.tsx
@@ -15,8 +15,7 @@ export default function Consultant() {
 
   return (
     <section id="team" ref={ref} className="overflow-hidden">
-      <div className="max-w-7xl mx-auto">
-        <div className="grid grid-cols-1 items-center py-4 bg-[var(--navy)]">
+        <div className="max-w-7xl grid grid-cols-1 items-center py-4 bg-[var(--navy)]">
           {/* Section label */}
           <p className="text-center text-lg font-bold tracking-[0.2em] uppercase text-[var(--primary)] ">
             {t(content.nav.consultants)}
@@ -102,7 +101,6 @@ export default function Consultant() {
             </div>
           </div>
         </div>
-      </div>
     </section>
   );
 }
